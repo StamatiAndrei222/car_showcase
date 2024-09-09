24badbb22eb9002243771ee8f1511a53db3aee1a
stamati <andreinustamati@gmail.com>
2024-09-09 17:09:16 +0300

    Rapid API Cars & Card Details

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,56 @@ import SearchBar from "@/components/SearchBar";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { fetchCars } from "@/utils";
+import { CarCard, ShowMore } from "@/components";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
-    
-      <main className="overflow-hidden">
-        <Hero />
 
-        <div className='mt-12 padding-x padding-y max-width' id='discover'>
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
           <p>Explore out cars you might like</p>
         </div>
-        </div>
-        <div className='home__filters'>
-          <SearchBar />
+      </div>
+      <div className='home__filters'>
+        <SearchBar />
 
-          <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
-          </div>
+        <div className='home__filter-container'>
+          <CustomFilter title='fuel' />
+          <CustomFilter title='year' />
         </div>
-      </main>
-    
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+
+
+         </div>
+    </main>
+
   );
 }
---
+++ components/ShowMore.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react'
+
+
+
+const ShowMore = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShowMore
